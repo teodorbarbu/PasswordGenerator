40e7f68d2deffa989326891e5ff312515774d678
teodorbarbu <teodorbarbu@yahoo.com>
2024-07-31 08:06:23 -0400

    Tracking Input State & Additional Properties
    Tracking Input State & Additional Properties

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,9 +11,27 @@ import { RouterOutlet } from '@angular/router';
 export class AppComponent {
   title = 'PasswordGenerator';
 
+  includeLetters = false;
+  includeNumbers = false;
+  includeSymbols = false;
   password = '';
 
+  onChangeUseLetters() {
+    this.includeLetters = !this.includeLetters;
+  }
+
+  onChangeUseNumbers() {
+    this.includeNumbers = !this.includeNumbers;
+  }
+
+  onChangeUseSymbols() {
+    this.includeSymbols = !this.includeSymbols;
+  }
+
   onButtonClick() {
+    console.log(this.includeLetters);
+    console.log(this.includeNumbers);
+    console.log(this.includeSymbols);
     this.password = 'MY PASSWORD!!!';
   }
   getPassword() {
